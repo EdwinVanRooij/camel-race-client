a5ec4c4ff11632ad3446135c071b9ca46e55c09f
Edwin van Rooij <edwinvrooij@gmail.com>
2017-06-17 14:03:10 +0200

    config update

--- app/src/main/java/io/github/edwinvanrooij/camelraceapp/Config.java
+++ app/src/main/java/io/github/edwinvanrooij/camelraceapp/Config.java
@@ -9,11 +9,13 @@ public class Config {
     public static final String GAME_ID = "url";
 
     public static final int CONNECTED_CHECKER_INTERVAL = 10; // in seconds
-    private final static String BACKEND_IP = "192.168.178.37 ";
+//    private final static String BACKEND_IP = "192.168.178.37 ";
+//    private final static String BACKEND_IP = "192.168.5.131";
+    private final static String BACKEND_IP = "88.159.34.253";
 //    private final static String BACKEND_IP = "192.168.5.115";
 //    private final static String BACKEND_IP = "172.17.0.1";
 
-    private final static int BACKEND_PORT = 8082;
+    private final static int BACKEND_PORT = 8085;
     private final static String BACKEND_PATH = "client";
 
     public final static String BACKEND_CONNECTION_URL = String.format("ws://%s:%s/%s", BACKEND_IP, BACKEND_PORT, BACKEND_PATH);
