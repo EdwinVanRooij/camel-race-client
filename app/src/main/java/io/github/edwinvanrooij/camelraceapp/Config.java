64e9d6d57134df1abbbdb8de4f3ef6295a8138d7
Edwin van Rooij <edwinvrooij@gmail.com>
2017-07-17 14:56:27 +0200

    uhenoa

--- app/src/main/java/io/github/edwinvanrooij/camelraceapp/Config.java
+++ app/src/main/java/io/github/edwinvanrooij/camelraceapp/Config.java
@@ -11,8 +11,10 @@ public class Config {
     public static final int CONNECTED_CHECKER_INTERVAL = 10; // in seconds
 //    private final static String BACKEND_IP = "192.168.178.37 "; // idk
 //    private final static String BACKEND_IP = "192.168.5.131"; // idk
-//    private final static String BACKEND_IP = "88.159.34.253"; // production
-    private final static String BACKEND_IP = "192.168.5.115"; // home laptop
+
+
+    private final static String BACKEND_IP = "88.159.34.253"; // production
+//    private final static String BACKEND_IP = "192.168.5.115"; // home laptop
 
     private final static int BACKEND_PORT = 8085;
     private final static String BACKEND_PATH = "client";
